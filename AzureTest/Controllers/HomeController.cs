cf6541aabf83d1b791ec7e10018c8081dfb6706c
Daniel Nordmark <daniel.git@yodapp.se>
2018-05-15 14:39:09 +0200

    Initial commit by D

---
+++ AzureTest/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AzureTest.Models;
+
+namespace AzureTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var nerd = new Nerd
+            {
+                Age = 38,
+                Name = "Daniel Nordmark"
+            };
+
+
+            return View(nerd);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
